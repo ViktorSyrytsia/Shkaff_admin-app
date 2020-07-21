3aa2c35084cecd99ef4a3dc4d902896ff32b073a
Oleh-Viktor <syrytsiaviktor@gmail.com>
2020-07-21 22:50:05 +0300

    add subcategory to root reducer

--- src/redux/root.saga.js
+++ src/redux/root.saga.js
@@ -1,7 +1,9 @@
 import { all } from 'redux-saga/effects';
 
 import categorySaga from "./category/category.saga";
+import subcategorySaga from "./subcategory/subcategory.saga";
 
 export default function* rootSaga() {
     yield all([categorySaga()]);
+    yield all([subcategorySaga()]);
 }
